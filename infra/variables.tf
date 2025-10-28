acb893657b2923b7c72bcf9a9e795bbb862d0c5d
Priyangshu Chakraborty <142937132+priyangshu-chakraborty@users.noreply.github.com>
2025-10-28 23:39:14 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,7 +3,7 @@ variable "aws_region"   { default = "us-east-2" } # Ohio
 variable "aws_profile"  { default = "default" }
 variable "vpc_cidr"     { default = "10.0.0.0/16" }
 variable "public_subnet_cidr" { default = "10.0.1.0/24" }
-variable "instance_type" { default = "t2.micro" }
+variable "instance_type" { default = "t3.micro" }
 variable "key_name" {
   description = "EC2 key pair name"
   default     = "win-pwd"
